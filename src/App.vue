38c02800dc9a034a3c55b11b0bae8b01a48f8b80
Parth Mudgal <artpar@gmial.com>
2020-10-22 12:12:59 +0530

    daptin office suite dedicated repo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="q-app">
+    <router-view/>
+
+  </div>
+</template>
+<script>
+
+  import {mapGetters, mapActions} from 'vuex';
+  import "fontsource-source-sans-pro"
+
+  export default {
+    data() {
+      return {
+        showLogin: false,
+        ...mapGetters(['loggedIn'])
+      }
+    },
+    methods: {
+      ...mapActions([])
+    },
+    name: 'App',
+    mounted() {
+      console.log("Loaded app user is ", this.loggedIn());
+      if (!this.loggedIn()) {
+        this.$router.push("/login")
+      }
+
+    }
+  }
+</script>
---
+++ src/pages/UserApps/UserAppsList.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page-container>
+    <user-header-bar :buttons="{}"></user-header-bar>
+  </q-page-container>
+
+
+</template>
+<script>
+export default {
+  name: "UsersAppsList",
+  mounted() {
+    console.log("Mounted user apps")
+  }
+}
+</script>
